48c3bc5c0f0430aa41f720e66c384ba6ba3e5c39
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2026-05-08 23:22:49 +0700

    linkage

--- CppCon_2020/The_Structure_of_a_Program/function.cpp
+++ CppCon_2020/The_Structure_of_a_Program/function.cpp
@@ -1,1 +1,13 @@
 #include "header.h"
+// external linkage: co the truy cap tu file cpp khac
+int public_var = 10;
+void doPublicWork(){
+    std::cout << "Public work is being done...\n";
+}
+// internal linkage: chi truy cap duoc trong file cpp nay
+static int private_var = 20;
+// no linkage
+void localFunction(){
+    int secret_var = 30; // chi truy cap duoc trong ham nay
+    std::cout << "This is a local function...\n";
+}
--- CppCon_2020/The_Structure_of_a_Program/main.cpp
+++ CppCon_2020/The_Structure_of_a_Program/main.cpp
@@ -1,8 +1,17 @@
 #include "header.h"
+// xin phep linker sai ke
+extern int public_var;
+extern void doPublicWork();
+ //extern int private_var; 
+// loi: khong the truy cap bien static tu file cpp khac
 int main(){
     User user;
     user.printName();
     std::cout << "\n";
     user.doSomething();
+    //////////////////////
+    std:: cout << "\nPublic variable: " << public_var << "\n";
+    doPublicWork();
+    //std::cout << "Private variable (should not be accessible): " << private_var << "\n"; // loi: khong the truy cap bien static tu file cpp khac
     return 0;
 }

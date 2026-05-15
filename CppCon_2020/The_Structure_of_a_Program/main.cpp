a3bf1dfc6d1f5ffe18a4c270d10cbc26147cda7c
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2026-05-16 06:52:30 +0700

    name mangling

--- CppCon_2020/The_Structure_of_a_Program/main.cpp
+++ CppCon_2020/The_Structure_of_a_Program/main.cpp
@@ -4,6 +4,18 @@ extern int public_var;
 extern void doPublicWork();
  //extern int private_var; 
 // loi: khong the truy cap bien static tu file cpp khac
+// name mangling
+void doWork(int a){
+    std:: cout << "hehe";
+}
+void doWork(double a){
+    std:: cout << "haha";
+}
+extern "C"{
+    void do_Work(int a){
+        doWork(a);
+    } // ok
+}
 int main(){
     User user;
     user.printName();
@@ -12,6 +24,8 @@ int main(){
     //////////////////////
     std:: cout << "\nPublic variable: " << public_var << "\n";
     doPublicWork();
+    doWork(1); // hehe
+    doWork(1.0); // haha
     //std::cout << "Private variable (should not be accessible): " << private_var << "\n"; // loi: khong the truy cap bien static tu file cpp khac
     return 0;
 }
